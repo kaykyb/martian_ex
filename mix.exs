402df587f531a74e7f2962ca7fd3053ffdbc7c27
Kayky de Brito <oss@kaykyb.com>
2023-12-06 01:04:51 -0300

    Fix metadata for Hex publish

--- mix.exs
+++ mix.exs
@@ -5,11 +5,12 @@ defmodule MartianEx.MixProject do
     [
       app: :martian_ex,
       version: "0.1.0",
-      description: "MartianEx allows converting Markdown text to Notion API block objects",
-      licenses: ["MIT"],
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      package: package(),
+      deps: deps(),
+      name: "MartianEx",
+      description: description()
     ]
   end
 
@@ -25,7 +26,19 @@ defmodule MartianEx.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:earmark, "~> 1.4"}
+      {:earmark, "~> 1.4"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "MartianEx allows converting Markdown text to Notion API block objects"
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/kaykyb/martian_ex"}
     ]
   end
 end
